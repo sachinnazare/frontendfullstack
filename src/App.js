646fa458d44f351d58220a57090a49a230d42639
sachinnazare <sachinnazare58@gmail.com>
2023-10-02 00:05:36 -0700

    front end

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Appbar from './Component/Appbar';
+import Student from './Component/Student';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Appbar/>
+    <Student/>
+    </div>
+  );
+}
+
+export default App;
